d543997db74702e5d122f087b83c956764b4c069
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-12-14 21:12:54 -0700

    re init metrics after clearing

--- server/routes/metrics/monthly.ts
+++ server/routes/metrics/monthly.ts
@@ -1,13 +1,25 @@
-import { getRegistry } from '../../utils/metrics';
+import { getRegistry, initializeAllMetrics } from '../../utils/metrics';
 import { scopedLogger } from '../../utils/logger';
 
 const log = scopedLogger('metrics-monthly-endpoint');
 
+let isInitialized = false;
+
+async function ensureMetricsInitialized() {
+  if (!isInitialized) {
+    log.info('Initializing metrics from monthly endpoint...', { evt: 'init_start' });
+    await initializeAllMetrics();
+    isInitialized = true;
+    log.info('Metrics initialized from monthly endpoint', { evt: 'init_complete' });
+  }
+}
+
 export default defineEventHandler(async event => {
   try {
+    await ensureMetricsInitialized();
     // Get the monthly registry
     const monthlyRegistry = getRegistry('monthly');
-    
+
     const metrics = await monthlyRegistry.metrics();
     event.node.res.setHeader('Content-Type', monthlyRegistry.contentType);
     return metrics;
