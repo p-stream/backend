4939becabfaa33026bfe9fcb2d7a0fe307951a42
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-12-14 21:30:24 -0700

    fix lint and import error

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../generated/client';
+import { PrismaClient } from '../generated';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
@@ -9,6 +9,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({
+  adapter,
+} as any);
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
